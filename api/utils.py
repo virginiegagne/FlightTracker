f28d006a0aa8271fdf2c1e9fc4cc08a92cf04ad8
virginiegagne <98621965+virginiegagne@users.noreply.github.com>
2022-01-29 11:53:27 -0500

    This is my first commit! Yay!

---
+++ api/utils.py
@@ -0,0 +1,15 @@
+def solid_example_1(*, example_param_1: str, example_param_2: int):
+    """
+
+    :param example_param_1: this is the first parameter
+    :param example_param_2:
+    :return:
+    """
+
+    #TODO: THIS IS A TODO. You must do this.
+
+def solid_example_2(example_param_1: float) -> int:
+    return 2
+
+def solid_example_3(*, example_param_1: float = 4.0):
+    pass
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+from api import utils
+
+
+
 def main_wrapper():
     #attributes are a thing
     print(f"This is the start of our python project. This function's name is {main_wrapper.__name__}")
-#os
+
+
     #Code here
+    utils.solid_example_1(example_param_1 = 'hi', example_param_2 = 2)
+    utils.solid_example_2()
+    utils.solid_example_3()
+
 
     print("This is the end of our python project")
 
