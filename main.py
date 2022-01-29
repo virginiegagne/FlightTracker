390d550e57d654b287648e5a48efd68312fda959
virginiegagne <98621965+virginiegagne@users.noreply.github.com>
2022-01-29 11:04:13 -0500

    This is my first commit! Yay!

---
+++ main.py
@@ -0,0 +1,12 @@
+def main_wrapper():
+    #attributes are a thing
+    print(f"This is the start of our python project. This function's name is {main_wrapper.__name__}")
+
+    #Code here
+
+    print("This is the end of our python project")
+
+    pass
+
+if __name__ == "__main__":
+    main_wrapper()
